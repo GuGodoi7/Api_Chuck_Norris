0fc6c8dd8c37a25fe1176a04c98116aad755b1e2
Gustavo Godoi da Silva <127133335+GuGodoi7@users.noreply.github.com>
2023-10-22 02:16:19 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,64 @@
-// Importa o estilo CSS para o componente.
 import "./App.css";
-
-// Importa hooks do React para usar no componente.
 import { useEffect, useState } from "react";
+import Botao from "./Componentes/Botao.js"; // Importe o componente Botao
 
 function App() {
-  // Estado para armazenar a piada atual.
   const [joke, setJoke] = useState("");
-
-  // Estado para armazenar as piadas favoritas.
   const [favorites, setFavorites] = useState([]);
-
-  // Estado para controlar o carregamento das piadas.
   const [isLoading, setIsLoading] = useState(false);
-
-  // Estado para armazenar o histórico de piadas.
   const [jokeHistory, setJokeHistory] = useState([]);
-
-  // Estado para controlar o índice da piada atual no histórico.
   const [currentJokeIndex, setCurrentJokeIndex] = useState(-1);
 
-  // Função para carregar as piadas favoritas do localStorage.
   const loadFavoritesFromLocalStorage = () => {
-    const storedFavorites = localStorage.getItem("favorites");
-    if (storedFavorites) {
-      setFavorites(JSON.parse(storedFavorites));
+    const storedFavoritas = localStorage.getItem("favorites");
+    if (storedFavoritas) {
+      setFavorites(JSON.parse(storedFavoritas));
     }
   };
 
-  // Função para buscar uma nova piada da API.
   const fetchJoke = () => {
     setIsLoading(true);
     fetch("https://api.chucknorris.io/jokes/random")
       .then((response) => response.json())
       .then((data) => {
-        // Cria uma cópia do histórico de piadas e adiciona a nova piada.
         const newJokeHistory = [...jokeHistory];
         newJokeHistory.push(data.value);
         setJokeHistory(newJokeHistory);
-
-        // Atualiza o índice da piada atual.
-        setCurrentJokeIndex(newJokeHistory.length - 1);
-
-        // Define a nova piada.
+        setCurrentJokeIndex(newJokeHistory.length);
         setJoke(data.value);
       })
       .catch((error) => console.error(error))
       .finally(() => setIsLoading(false));
   };
 
-  // Efeito para carregar as piadas favoritas do localStorage e buscar uma nova piada ao montar o componente.
   useEffect(() => {
     loadFavoritesFromLocalStorage();
     fetchJoke();
   }, []);
 
-  // Função para adicionar a piada atual aos favoritos.
   const handleLike = () => {
     const newFavorites = [...favorites, joke];
     setFavorites(newFavorites);
-
-    // Salva as piadas favoritas no localStorage.
     localStorage.setItem("favorites", JSON.stringify(newFavorites));
   };
 
-  // Função para remover uma piada da lista de favoritos.
   const handleRemove = (index) => {
     if (
       window.confirm(
-        "Tem certeza que deseja remover esta piada da lista de favoritos?",
+        "Tem certeza que deseja remover esta piada da lista de favoritos?"
       )
     ) {
       const newFavorites = [...favorites];
       newFavorites.splice(index, 1);
       setFavorites(newFavorites);
-
-      // Atualiza as piadas favoritas no localStorage.
       localStorage.setItem("favorites", JSON.stringify(newFavorites));
     }
   };
 
-  // Função para buscar a próxima piada.
   const handleNextJoke = () => {
     fetchJoke();
   };
 
-  // Função para voltar para a piada anterior no histórico.
   const handlePreviousJoke = () => {
     if (currentJokeIndex > 0) {
       const previousJokeIndex = currentJokeIndex - 1;
@@ -94,25 +67,18 @@ function App() {
     }
   };
 
-  // Renderiza o componente com elementos HTML.
   return (
     <div className="container">
       <img src="/chucknorris_logo.png" alt="chucknorris_logo" />
       <h1>Chuck Norris Joke</h1>
       {isLoading ? (
-        <p>...</p>
+        <p></p>
       ) : (
         <>
           <p>{joke}</p>
-          <button className="button-3d" onClick={handlePreviousJoke}>
-            Voltar
-          </button>
-          <button className="button-3d" onClick={handleLike}>
-            Like
-          </button>
-          <button className="button-3d" onClick={handleNextJoke}>
-            Próximo
-          </button>
+          <Botao texto="Voltar" onClick={handlePreviousJoke} />
+          <Botao texto="Like" onClick={handleLike} />
+          <Botao texto="Próximo" onClick={handleNextJoke} />
         </>
       )}
       <h2>Favoritos</h2>
@@ -120,9 +86,7 @@ function App() {
         {favorites.map((favJoke, index) => (
           <li key={index}>
             {favJoke}
-            <button className="button-3d" onClick={() => handleRemove(index)}>
-              Remover
-            </button>
+            <Botao texto="Remover" onClick={() => handleRemove(index)} />
           </li>
         ))}
       </ul>
